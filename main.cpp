794a0326c04e7e7bd1bd14e55475b1087d271f07
Cache Makers <Image15@iMac-D.local>
2017-01-26 18:05:29 -0700

    day 3

--- main.cpp
+++ main.cpp
@@ -1,17 +1,22 @@
 #include <iostream>
+
+char board[3][3] = {{' ',' ',' '}{}}
+
 void printBoard(char board[2][2] ){
   for(int i=0;i<3;i++){
     for(int j=0;j<3;j++){
       std::cout << board[i][j];
     }
+    std::cout << std::endl;
   }
   }
+
 int main(){
   int turn=0;
   bool gameOver = false;
   int rowIn;
   int colIn;
-  char board [2][2];
+
 
   std::cout<< "Welcome to the game!"<< std::endl;
   while(gameOver==false){
